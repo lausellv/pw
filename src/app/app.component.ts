35dea3216cabaaf94f2d91c1dba0a03092579a36
Victor <lausellv@yahoo.com>
2021-09-29 19:08:29 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+  onButtonClick() {
+    console.log(this.includeLetters, this.includeLetters, this.includeSymbols);
+    this.password = 'Your Password is ...';
+  }
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+onChangeUseNumbers() {
+  this.includeNumbers = !this.includeNumbers;
+}
+
+onChangeUseSymbols() {
+  this.includeSymbols = !this.includeSymbols;
+}
+
 }
